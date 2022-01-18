276944d54440476ab92b885a00f6a0dd5dede7d9
徐富强 <xufuqiang@nuoboauto.com>
2022-01-18 15:19:57 +0800

    opt: 新增接口管理

---
+++ lib-player/src/main/java/com/noboauto/applet/video/MediaFactory.java
@@ -0,0 +1,18 @@
+package com.noboauto.applet.video;
+
+/**
+ * desc
+ *
+ * @author Tomcat
+ * @date 2022/1/18 15:10
+ */
+public class MediaFactory {
+
+    public static VideoContext createVideoContext() {
+
+        return new VideoContext();
+
+    }
+
+
+}
---
+++ lib-player/src/main/java/com/noboauto/applet/video/VideoContext.java
@@ -0,0 +1,85 @@
+package com.noboauto.applet.video;
+
+/**
+ * desc
+ *
+ * @author Tomcat
+ * @date 2022/1/18 15:12
+ */
+public class VideoContext {
+    /**
+     * 退出后台音频播放模式。
+     */
+    void exitBackgroundPlayback() {
+
+    }
+    /**
+     * 退出全屏
+     */
+    void exitFullScreen() {
+
+    }
+
+    /**
+     * 退出小窗，该方法可在任意页面调用
+     */
+    void exitPictureInPicture() {
+
+    }
+
+    /**
+     * 暂停视频
+     */
+    void pause() {
+
+    }
+    /**
+     * 开始播放
+     */
+    void play() {
+
+    }
+
+    /**
+     * 设置倍速播放
+     */
+    void playbackRate() {
+
+    }
+
+    /**
+     * 进入后台音频播放模式
+     */
+    void requestBackgroundPlayback() {
+
+    }
+
+    /**
+     * 进入全屏。若有自定义内容需在全屏时展示，需将内容节点放置到 video 节点内
+     */
+    void requestFullScreen() {
+
+    }
+
+    /**
+     * 跳转到指定位置
+     */
+    void seek() {
+
+    }
+
+    /**
+     * 发送弹幕
+     */
+    void sendDanmu() {
+
+    }
+
+
+    /**
+     * 停止视频
+     */
+    void stop() {
+
+    }
+}
